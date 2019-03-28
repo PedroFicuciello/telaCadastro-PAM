5df97ebeea710988829e35eee81bcc0217c8485b
PedroFicuciello <phficuciello@gmail.com>
2019-03-28 04:13:19 -0300

    Tela de Cadastro em Java

---
+++ app/src/main/java/com/example/aluno/telacadastro/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.aluno.telacadastro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private  EditText nome;
+    private  EditText sobrenome;
+    private  EditText email;
+    private  EditText nomeUsuario;
+    private  EditText senha;
+    private  EditText confirmarSenha;
+    private  Button confirmar;
+    private  Button limpar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nome = (EditText) findViewById(R.id.nome);
+        sobrenome = (EditText) findViewById(R.id.sobrenome);
+        email = (EditText) findViewById(R.id.email);
+        nomeUsuario = (EditText) findViewById(R.id.nomeUsuario);
+        senha = (EditText) findViewById(R.id.senha);
+        confirmarSenha = (EditText) findViewById(R.id.confirmarSenha);
+        confirmar = (Button) findViewById(R.id.btnConfirmar);
+        limpar = (Button) findViewById(R.id.btnLimpar);
+
+
+        confirmar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                //ESCREVER CÓDIGOOOOOOOOOOOOO
+
+            }
+        });
+
+        limpar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick (View v) {
+
+                nome.setText(" ");
+                sobrenome.setText(" ");
+                email.setText(" ");
+                nomeUsuario.setText(" ");
+                senha.setText(" ");
+                confirmarSenha.setText(" ");
+            }
+        });
+    }
+}
